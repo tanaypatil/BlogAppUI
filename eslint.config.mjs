f1768ce9007f71391edfeab0028332bfa93a10fe
tanaypatil <tanaypatil197@gmail.com>
2025-07-22 21:26:55 +0530

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,8 @@ export default {
       document: 'readonly',
       localStorage: 'readonly',
       sessionStorage: 'readonly',
+      FormData: 'readonly',
+      Element: 'readonly',
       navigator: 'readonly',
       fetch: 'readonly',
       setTimeout: 'readonly',
