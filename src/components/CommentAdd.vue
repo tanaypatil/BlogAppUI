df30f9cc2d3b9c9b45dc7188745044585153e80b
tanaypatil <tanaypatil197@gmail.com>
2025-07-29 22:21:41 +0530

    Added comments ui components

---
+++ src/components/CommentAdd.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['post'])
+
+const comment = ref('')
+
+function emitPost() {
+  if (!comment.value.trim()) return
+  emit('post', comment.value.trim())
+  comment.value = ''
+}
+</script>
+
+<template>
+  <v-row align="center" no-gutters class="pa-3">
+    <v-col cols="4" offset="4">
+      <div class="pa-2 rounded bg-blue-grey-lighten-1">
+        <v-text-field
+          v-model="comment"
+          dense
+          variant="underlined"
+          label="Add a comment"
+          clearable
+          hide-details
+          @keyup.enter="emitPost"
+        />
+      </div>
+    </v-col>
+    <v-col cols="1">
+      <div class="pa-2 rounded">
+        <v-btn color="primary" icon :disabled="!comment.trim()" @click="emitPost">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+      </div>
+    </v-col>
+  </v-row>
+</template>
---
+++ src/components/CommentCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-card
+    elevation="2"
+  >
+    <template v-slot:title>Author</template>
+    <template v-slot:text>This is a good blog</template>
+    <template v-slot:subtitle>12:00 PM</template>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
